cd52f221bbdb4dd5f33a924b5e81756162f5a6ad
Kurt Seifried <kurt@seifried.org>
2024-04-22 22:15:42 -0600

    sms/mms gateways

---
+++ email-sms-mms-gateways/get-files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/mfitzp/List_of_SMS_gateways/master/email2sms.csv
+sed 's/,/\n/g' email2sms.csv  | grep @ | cut -d"@" -f2 | cut -d" " -f1 | sort | uniq > email2sms.csv.stripped
+
+wget https://raw.githubusercontent.com/cubiclesoft/email_sms_mms_gateways/master/sms_mms_gateways.txt
+sed 's/"/\n/g' sms_mms_gateways.txt  | grep @ | cut -d"@" -f2 | cut -d" " -f1 | sort | uniq > sms_mms_gateways.txt.stripped
+
+cat email2sms.csv.stripped sms_mms_gateways.txt.stripped | sort | uniq > consolidated-sms-mms.txt
